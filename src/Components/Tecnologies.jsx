3640103d3e745e73ee82fe4e85c2938bd21e3a6d
faithyyyy <faithfortune6@gmail.com>
2023-03-30 20:44:44 +0100

    Tech Stacks change

--- src/Components/Tecnologies.jsx
+++ src/Components/Tecnologies.jsx
@@ -10,7 +10,7 @@ function Tech() {
     <>
       <section className="px-5 py-16 md:pt-6  xl:pt-28 max-w-6xl mx-auto xl:px-0">
         <h2 className="font-[Recoleta] font-[900] text-4xl sm:text-5xl md:text-6xl xl:text-7xl leading-[1.3] sm:leading-[1.3] md:leading-[1.4] xl:leading-[1.2]  max-w-4xl mb-1 ">
-          Technologies
+          Tech Stack
         </h2>
         <p className="text-gray-500 font-openSans text-xl mb-10">
           I've worked with a range of technologies in the web development
